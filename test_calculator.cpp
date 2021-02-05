bc4ca208f5c92977144bf14c0d4ab4baf7432c94
stepin104890 <theresmary98@gmail.com>
2021-02-05 11:22:01 +0530

    updated

--- test_calculator.cpp
+++ test_calculator.cpp
@@ -4,14 +4,14 @@
 #include <calculator.h>
 
 TEST(CalculatorTest,add){
-    EXPECT_EQ(100,add(80,20));
-    EXPECT_EQ(60,add(80,-20));
-    EXPECT_EQ(-50,add(-20,-30));
+    EXPECT_EQ(11,add(5,6));
+    EXPECT_EQ(12,add(6,6));
+    EXPECT_EQ(13,add(7,6));
 }
 TEST(CalculatorTest, sub){
-    EXPECT_EQ(60,sub(80,20));
+    EXPECT_EQ(80,sub(100,20));
     EXPECT_EQ(100,sub(80,-20));
-    EXPECT_EQ(-50,sub(-20,-30));
+    EXPECT_EQ(50,sub(100,50));
 }
 TEST(CalculatorTest,mul){
     EXPECT_EQ(1600,mul(80,20));
@@ -19,9 +19,9 @@ TEST(CalculatorTest,mul){
     EXPECT_EQ(600,mul(-20,-30));
 }
 TEST(CalculatorTest,add){
-    EXPECT_EQ(4,division(80,20));
-    EXPECT_EQ(-4,division(80,-20));
-    EXPECT_EQ(0,division(-20,-30));
+    EXPECT_EQ(4,divn(80,20));
+    EXPECT_EQ(-4,divn(80,-20));
+    EXPECT_EQ(0,divn(-20,-30));
 }
 
 int main(int argc,char **argv){
