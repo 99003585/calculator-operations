2f1abd427eb2539ed10673e0c0b04a9a554dd80c
stepin104890 <theresmary98@gmail.com>
2021-02-05 16:18:56 +0530

    test file updated

--- test_calculator.cpp
+++ test_calculator.cpp
@@ -5,23 +5,23 @@
 
 TEST(CalculatorTest,add){
     EXPECT_EQ(11,add(5,6));
-    EXPECT_EQ(12,add(6,6));
-    EXPECT_EQ(13,add(7,6));
+    EXPECT_EQ(0,add(0,0));
+    EXPECT_EQ(-13,add(-7,-6));
 }
 TEST(CalculatorTest, sub){
     EXPECT_EQ(80,sub(100,20));
-    EXPECT_EQ(100,sub(80,-20));
-    EXPECT_EQ(50,sub(100,50));
+    EXPECT_EQ(-60,sub(-80,-20));
+    EXPECT_EQ(0,sub(0,0);
 }
 TEST(CalculatorTest,mul){
     EXPECT_EQ(1600,mul(80,20));
-    EXPECT_EQ(-1600,mul(80,-20));
-    EXPECT_EQ(600,mul(-20,-30));
+    EXPECT_EQ(1600,mul(-80,-20));
+    EXPECT_EQ(0,mul(0,0));
 }
 TEST(CalculatorTest,add){
-    EXPECT_EQ(4,divn(80,20));
-    EXPECT_EQ(-4,divn(80,-20));
-    EXPECT_EQ(0,divn(-20,-30));
+    EXPECT_EQ(5,divn(50,10));
+    EXPECT_EQ(1,divn(-20,-20));
+    EXPECT_EQ(0,divn(-20,0));
 }
 
 int main(int argc,char **argv){
