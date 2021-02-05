7981d45ee4aa8e415262df181dae6504a2d3e8ec
stepin104890 <theresmary98@gmail.com>
2021-02-05 11:10:35 +0530

    changed branch

---
+++ calculator.h
@@ -0,0 +1,11 @@
+#ifndef __CALCULATOR.H__
+#define __CALCULATOR.H__
+
+#include<iostream>
+
+
+int add(int val1, int val2);
+int sub(int val1, int val2);
+int mul(int val1, int val2);
+int divn(int val1, int val2);
+#endif
